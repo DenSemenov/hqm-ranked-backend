aa153073aeb2f3649c35d879a9292ebd103e60f6
Den Semenov <dsmnvofficial@gmail.com>
2024-05-02 12:14:37 +0300

    AddedHeartbeat

---
+++ hqm-ranked-backend/Migrations/20240502091416_AddedHeartbeat.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace hqm_ranked_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedHeartbeat : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "BlueScore",
+                table: "Servers",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "LoggedIn",
+                table: "Servers",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Period",
+                table: "Servers",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "RedScore",
+                table: "Servers",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "State",
+                table: "Servers",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Time",
+                table: "Servers",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BlueScore",
+                table: "Servers");
+
+            migrationBuilder.DropColumn(
+                name: "LoggedIn",
+                table: "Servers");
+
+            migrationBuilder.DropColumn(
+                name: "Period",
+                table: "Servers");
+
+            migrationBuilder.DropColumn(
+                name: "RedScore",
+                table: "Servers");
+
+            migrationBuilder.DropColumn(
+                name: "State",
+                table: "Servers");
+
+            migrationBuilder.DropColumn(
+                name: "Time",
+                table: "Servers");
+        }
+    }
+}
