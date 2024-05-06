59cdd6b8c300f1299b3317e7537a29a6e12c7dcf
Denis Semenov <denis.semenov@pix.ru>
2024-05-06 11:11:57 +0300

    Changed auth settings

--- hqm-ranked-backend/Common/AuthOptions.cs
+++ hqm-ranked-backend/Common/AuthOptions.cs
@@ -5,8 +5,8 @@ namespace hqm_ranked_backend.Common
 {
     public class AuthOptions
     {
-        public const string ISSUER = "HqmAuthServer";
-        public const string AUDIENCE = "HqmAuthClient";
+        public const string ISSUER = "HqmRankedAuthServer";
+        public const string AUDIENCE = "HqmRankedAuthClient";
         const string KEY = "0925bf65-7698-4f48-bcba-f7228d6f88bb"; 
         public const int LIFETIME = 30;
         public static SymmetricSecurityKey GetSymmetricSecurityKey()
