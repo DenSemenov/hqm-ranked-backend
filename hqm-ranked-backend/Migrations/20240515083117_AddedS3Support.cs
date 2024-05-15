1c2969fdaa9a3cae5155bcd1d94e5390f0cff4ac
Denis Semenov <denis.semenov@pix.ru>
2024-05-15 11:31:27 +0300

    Added s3 settings

---
+++ hqm-ranked-backend/Migrations/20240515083117_AddedS3Support.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace hqm_ranked_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedS3Support : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "S3Bucket",
+                table: "Settings",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "S3Domain",
+                table: "Settings",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "S3Key",
+                table: "Settings",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "S3User",
+                table: "Settings",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "S3Bucket",
+                table: "Settings");
+
+            migrationBuilder.DropColumn(
+                name: "S3Domain",
+                table: "Settings");
+
+            migrationBuilder.DropColumn(
+                name: "S3Key",
+                table: "Settings");
+
+            migrationBuilder.DropColumn(
+                name: "S3User",
+                table: "Settings");
+        }
+    }
+}
