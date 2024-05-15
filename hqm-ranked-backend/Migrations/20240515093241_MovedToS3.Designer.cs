4e7ff9006a57cf3134f7a9cbaf9a9c573a8959f5
Denis Semenov <denis.semenov@pix.ru>
2024-05-15 12:32:55 +0300

    Moved to s3 migration

--- hqm-ranked-backend/Migrations/20240515093241_MovedToS3.Designer.cs
+++ hqm-ranked-backend/Migrations/20240515093241_MovedToS3.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using hqm_ranked_backend.Models.DbModels;
@@ -11,9 +12,11 @@ using hqm_ranked_backend.Models.DbModels;
 namespace hqm_ranked_backend.Migrations
 {
     [DbContext(typeof(RankedDb))]
-    partial class RankedDbModelSnapshot : ModelSnapshot
+    [Migration("20240515093241_MovedToS3")]
+    partial class MovedToS3
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ hqm-ranked-backend/Migrations/20240515093241_MovedToS3.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace hqm_ranked_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class MovedToS3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Data",
+                table: "ReplayData");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Url",
+                table: "ReplayData",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Url",
+                table: "ReplayData");
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Data",
+                table: "ReplayData",
+                type: "bytea",
+                nullable: false,
+                defaultValue: new byte[0]);
+        }
+    }
+}
