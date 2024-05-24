ed20ccc82aaecc1b40983fc8651be1710ffb5e1e
Denis Semenov <denis.semenov@pix.ru>
2024-05-24 09:44:35 +0300

    One more fix

--- hqm-ranked-backend/Startup.cs
+++ hqm-ranked-backend/Startup.cs
@@ -136,8 +136,6 @@ namespace hqm_ranked_backend
             var replayService = scope.ServiceProvider.GetRequiredService<IReplayService>() as ReplayService;
             RecurringJob.AddOrUpdate("RemoveOldReplays", () => replayService.RemoveOldReplays(), Cron.Daily);
             RecurringJob.AddOrUpdate("ParseReplays", () => replayService.ParseAllReplays(), Cron.Daily);
-
-            replayService.ParseAllReplays();
         }
     }
 }
